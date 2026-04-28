f86d9e8eb064213d42a30f1c69d0e8030bfa4a6d
8wali8 <ujjawalprasad111@gmail.com>
2026-04-28 15:58:00 -0400

    parse twitch irc messages

---
+++ chat-service/src/main/java/com/streamsense/chatservice/twitch/TwitchIrcChatMessage.java
@@ -0,0 +1,9 @@
+package com.streamsense.chatservice.twitch;
+
+public record TwitchIrcChatMessage(
+        String channel,
+        String user,
+        String message,
+        String messageId,
+        long timestamp) {
+}
